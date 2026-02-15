b5c0e1fad544c0e2565d35ba9c3711263cff74d0
PrabhakarYdv <prabhakarkr802@gmail.com>
2026-02-16 02:51:19 +0530

    Home UI

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import heroImage from '../assets/hero.gif'
 
 function Home() {
     return (
         <>
-            <h1>Welcome to E-Notebook</h1>
+            <div className="hero">
+                <img src={heroImage} alt="Image" />
+                <h1>E-NoteBook</h1>
+                <p>is a <span>Secure Digital NoteBook</span> in the cloud.That keeps your ideas <span>Safe</span> and <span>Accessible</span> anytime,
+                    Manage your notes effortlessly. With seamless <span>Signup</span> &amp; <span>Login</span> authentication with full <span>CRUD</span> functionality — <span>Fast, Simple, and Protected</span>.
+                    Your ideas deserve <span>Security</span> and <span>Simplicity</span>.
+                    All notes are organized efficiently to help you stay productive.
+                    Designed to keep your thoughts organized.
+                </p>
+
+                <div className="hero-btns">
+                    <button className="signup-btn" type="button">Signup</button>
+                    <button  className="login-btn" type="button">Login</button>
+                </div>
+            </div>
         </>
     )
 }
