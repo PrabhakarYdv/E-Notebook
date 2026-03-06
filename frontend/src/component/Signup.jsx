b53bcc27a89616f30133e2b95d7202cc157d9638
PrabhakarYdv <prabhakarkr802@gmail.com>
2026-03-06 23:39:44 +0530

    Signup UI

--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import userLogo from '../assets/user.gif'
+import logo from '../assets/task_loader.gif'
 
 function Signup() {
   return (
@@ -6,8 +8,20 @@ function Signup() {
       <div className="signup">
         <div className="signup_box">
           <div className="left">
+          <img src={logo} alt="task"/>
+            <h1>Welcome to <span>E-NoteBook</span>!</h1>
+            <p>Create your secure <span>E-NoteBook</span> and access your notes from anywhere</p>
           </div>
           <div className="right">
+            <img src={userLogo} alt="user" />
+            <form>
+              <input name="name" type="text" placeholder="Enter your name" />
+              <input name="email" type="email" placeholder="Enter your email" />
+              <input name="password" type="password" placeholder="Create a password" />
+              <input name="confirmPassword" type="password" placeholder="Enter password again" />
+              <button type="submit">Signup</button>
+            </form>
+            <p>Already have an account? <span>Login</span></p>
           </div>
         </div>
       </div>
